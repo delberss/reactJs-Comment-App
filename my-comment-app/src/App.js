34fc8ff66999795f890750802fa128e6402e6a16
Delber Soares <delberss@hotmail.com>
2022-04-22 20:42:29 -0300

    Projeto criado

---
+++ my-comment-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Comment from "./components/Comment";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Comments App!</h1>
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-comment-app/src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import "./Comment.css";
+
+const Comment = () => {
+    return(
+        <div className="comment">
+            <h2>Delber Soares</h2>
+            <p>Aqui é meu Comment Component.</p>
+            <p>22/04/2022</p>
+        </div>
+    );
+};
+
+export default Comment;
